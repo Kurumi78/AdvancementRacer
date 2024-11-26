d1103d706032206e574e4d99ce1f5049b17d8e12
Kurumi78 <kurumitk78@gmail.com>
2024-11-26 15:46:59 -0600

    Initial Commit

---
+++ src/main/java/com/gmail/kurumitk78/advancementRacer/AdvancementRacer.java
@@ -0,0 +1,17 @@
+package com.gmail.kurumitk78.advancementRacer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancementRacer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
